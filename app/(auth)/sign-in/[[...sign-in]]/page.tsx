9de7d034c754eb582a73ddd36214f4b6fb9e6c8a
victortosin02 <victor.oladejo@cloudware.ng>
2023-10-27 19:55:42 +0100

    explore-ai

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+// import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <SignIn />
-    </div>
-  );
-}
+// export default function Page() {
+//   return (
+//     <div className="w-screen h-screen flex justify-center items-center">
+//       <SignIn />
+//     </div>
+//   );
+// }
