52474bf9559f5d77933e232def0887cfc2bea06e
victortosin02 <victor.oladejo@cloudware.ng>
2023-10-28 11:45:13 +0100

    explore-ai

--- app/components/OrderSuccess.tsx
+++ app/components/OrderSuccess.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import { useCartStore } from "@/store/useCartStore";
-import { useClerk } from "@clerk/nextjs";
+// import { useClerk } from "@clerk/nextjs";
 
 const OrderSuccess = () => {
   const cartStore = useCartStore();
-  const { user } = useClerk();
+  // const { user } = useClerk();
 
   useEffect(() => {
     cartStore.setPaymentIntent("");
@@ -15,7 +15,7 @@ const OrderSuccess = () => {
     <>
       <button onClick={() => cartStore.setCheckout("cart")}>Back to store</button>
       <div className="w-full h-full flex justify-center items-center flex-col">
-        <h1 className="font-bold font-xl">Thanks {user?.username} for your order</h1>
+        {/* <h1 className="font-bold font-xl">Thanks {user?.username} for your order</h1> */}
         <p className="font-medium">
           You can view your order history <a href="/orders">here</a>
         </p>
